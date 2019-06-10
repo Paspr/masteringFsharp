20e9aed339233cc48f9b1aa59810edaa8c629604
Pavel Osminin <pav.osminin@gmail.com>
2019-06-11 02:41:47 +0500

    initial version

---
+++ 01_02/F0102.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+
+    let g n = n + 5 // int
+
+    let gg = fun n -> n+5
+
+    let h (x,y) = System.Math.Sqrt(x*x+y*y)
+
+    0 // return an integer exit code
