f4aa8f21d62737d2b2ad982caa87a198390b2ced
Pavel Osminin <pav.osminin@gmail.com>
2019-08-03 00:18:58 +0500

    17 initial version

---
+++ 01_06/F0106.fs
@@ -0,0 +1,18 @@
+﻿
+// 17.1
+let rec pow = function
+ |(s, 0) -> ""
+ |(s, n) -> s + pow(s, n-1)
+
+// 17.2
+let rec isIthChar = function
+ |(s,n,c) when n >= String.length s -> false
+ |(s,n,c) when n < 0 -> false
+ |(s,n,c) when s.[n] = c -> true
+ |_ -> false
+
+// 17.3
+let rec occFromIth = function
+ |(s,n,c) when n >= String.length s -> 0
+ |(s,n,c) when s.[n] = c -> 1 + occFromIth(s, n + 1, c)
+ |_ -> 0
