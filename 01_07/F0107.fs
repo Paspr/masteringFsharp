0c02b063136e2fe004cc8e434c83660a52973e34
Pavel Osminin <pav.osminin@gmail.com>
2019-08-14 11:24:26 +0500

    fix in 20.3.3

--- 01_07/F0107.fs
+++ 01_07/F0107.fs
@@ -1,8 +1,9 @@
-﻿// 20.3.1
+﻿
+// 20.3.1
 let vat n x = x + x * (n/100.0)
 
 // 20.3.2
 let unvat n x = x / (1.0 + n/100.0)
 
 // 20.3.3
-let rec min f = if f=0 then f else min (f+1)
+let rec min f n = if f n=0 then n else min f (n+1)
