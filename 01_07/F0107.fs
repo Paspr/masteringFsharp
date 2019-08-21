1d84da305615ba2f87a8206d109bec34dbefa8ea
Pavel Osminin <pav.osminin@gmail.com>
2019-08-22 00:48:23 +0500

    fix in 20.3.3

--- 01_07/F0107.fs
+++ 01_07/F0107.fs
@@ -6,4 +6,5 @@ let vat (n: int) (x: float) :float = x + x *  float n/100.0
 let unvat n x = x / (1.0 + n/100.0)
 
 // 20.3.3
-let rec min f n = if f n=0 then n else min f (n+1)
+let rec helper f n = if f n = 0 then n else helper f (n + 1)
+let min f = helper f 0
