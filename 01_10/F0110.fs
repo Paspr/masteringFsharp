3aea8d572b553f8d6d245b15cef71128faf45ec1
Pavel Osminin <pav.osminin@gmail.com>
2019-09-09 02:11:44 +0500

    initial version

---
+++ 01_10/F0110.fs
@@ -0,0 +1,4 @@
+﻿// 24.4
+type TimeOfDay = { hours: int; minutes: int; f: string }
+
+let (.>.) x y = x.f > y.f || x.f = y.f && (x.hours = y.hours && x.minutes > y.minutes || x.hours <> y.hours && (x.hours = 12 || y.hours <> 12 && x.hours > y.hours))
