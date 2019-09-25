462028144d01bd6f5bda20269748d1f342921257
Pavel Osminin <pav.osminin@gmail.com>
2019-09-25 20:12:59 +0500

    edit the functions' order

--- 01_12/F0112.fs
+++ 01_12/F0112.fs
@@ -1,15 +1,16 @@
-﻿let rec dnto (x: int) =
+﻿// 34.1
+let rec upto (x: int) =
+ match x with
+ | 0 -> []
+ | _ -> upto (x-1) @ [x]
+ 
+// 34.2
+let rec dnto (x: int) =
  match x with
  | 0 -> []
  | _ ->  x :: dnto (x-1) 
 
-
-let rec upto (x: int) =
- match x with
- | 0 -> []
- | _ -> upto (x-1) @ [x] 
-
-
+// 34.3
 let rec evenn (x: int) =
  match x with
  | 0 -> []
