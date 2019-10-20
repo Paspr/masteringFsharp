eec773ac2742c552dc3d037bc0e16829d066fb9c
Pavel Osminin <pav.osminin@gmail.com>
2019-10-20 16:20:05 +0500

    initial version

---
+++ 01_16/F0116.fs
@@ -0,0 +1,7 @@
+﻿// 42.3
+let rec allSubsets n k =
+    match n, k with
+    | _, 0 -> Set.empty.Add(Set.empty)
+    | 0, _ -> Set.empty
+    | n, k -> Set.union (Set.map (fun s -> Set.add n s) (allSubsets (n-1) (k-1)))
+                (allSubsets (n-1) k)
