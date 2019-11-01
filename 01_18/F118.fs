a4e4c07e6b8efeec9ca4b48b142e9b9304ad48fe
Pavel Osminin <pav.osminin@gmail.com>
2019-11-01 18:19:10 +0500

    initial version

---
+++ 01_18/F118.fs
@@ -0,0 +1,25 @@
+﻿// 47.4.1
+let f n = 
+ if (n=0 || n=1) then 1
+ else
+    let mutable x = 1
+    let mutable counter = n
+    while counter > 0 do
+     x <- x * counter
+     counter <- counter - 1
+    x
+
+
+// 47.4.2
+let fibo n = 
+ if n=1 then 0
+ else
+   let mutable x = 1
+   let mutable y = 0
+   let mutable counter = n
+   while counter >= 3 do
+    let x1 = x + y
+    y <- x
+    x <- x1
+    counter <- counter - 1
+   x
