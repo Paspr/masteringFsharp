a8f0deb2390b79d1575856c358e85f94d0cdd438
Pavel Osminin <35937304+Paspr@users.noreply.github.com>
2019-11-17 01:57:02 +0500

    initial version

---
+++ 01_20/F120.fs
@@ -0,0 +1,15 @@
+﻿// 49.5.1
+let even_seq = Seq.append (Seq.singleton 0) (Seq.initInfinite (fun i -> 2 * i))
+
+// 49.5.2
+let fac_seq = Seq.initInfinite (fun i -> [1..i] |> List.fold (*) 1)
+
+// 49.5.3
+
+let rec sign n = seq {
+ yield -n
+ yield n
+ yield! sign (n+1)
+}
+
+let seq_seq = Seq.append (Seq.singleton 0) (sign 1)
