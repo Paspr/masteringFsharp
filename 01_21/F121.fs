23a0cca6322fea1fc23061629e609a2a780c5adf
Pavel Osminin <35937304+Paspr@users.noreply.github.com>
2019-11-29 00:55:58 +0500

    initial version

---
+++ 01_21/F121.fs
@@ -0,0 +1,16 @@
+﻿// 50.2.1
+let fac_seq = seq {
+    yield 1
+    yield! Seq.initInfinite (fun i -> [1..i] |> List.fold (*) 1) |> Seq.skip 1
+}
+
+// 50.2.2
+let seq_seq = seq {
+    let rec sign n = seq {
+            yield -n
+            yield n
+            yield! sign (n+1)
+    }
+    yield 0 
+    yield! sign 1
+}
